cce9dae5d31e85cbad27b5c8a39f9c88b5565d1e
Duane <duane_oli@hotmail.com>
2023-09-20 18:39:12 -0300

    refactor(health-check): Reduce route headers for bandwidth optimization

--- src/controllers/BaseController.ts
+++ src/controllers/BaseController.ts
@@ -1,9 +1,18 @@
-import { Controller, Get } from '@nestjs/common'
-
+import { Controller, Get, Res } from '@nestjs/common'
+import { Response } from 'express'
 @Controller()
 export class BaseController {
   @Get('health-check')
-  healthCheck(): string {
-    return 'Healthy!'
+  healthCheck(@Res() req: Response) {
+    if (req) {
+      req.sendDate = false
+      req.shouldKeepAlive = false
+      req.set('Etag', '1')
+      req.removeHeader('Connection')
+      req.removeHeader('X-Powered-By')
+      req.removeHeader('Access-Control-Allow-Origin')
+      req.set('Content-Type', 'text/plain')
+    }
+    req.send('Healthy!')
   }
 }
